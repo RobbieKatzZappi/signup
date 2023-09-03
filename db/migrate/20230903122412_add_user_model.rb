8dfc0f3b6796a82c918d2ca7de33db380f7950f6
Robbie Katz <robert.katz@zappistore.com>
2023-09-03 15:12:35 +0200

    introduce users

--- db/migrate/20230903122412_add_user_model.rb
+++ db/migrate/20230903122412_add_user_model.rb
@@ -1,4 +1,14 @@
 class AddUserModel < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+     t.string :first_name, null: false
+     t.string :last_name, null: false
+     t.datetime :date_of_birth, null: false
+     t.string :gender, null: false
+     t.string :university, null: false
+     t.string :password_digest, null: false
+
+     t.timestamps
+   end
   end
 end
